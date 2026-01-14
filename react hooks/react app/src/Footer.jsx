532a9818bb1bcc29b48deb9ab8f08a6a1c9f12f9
Monishkumar15 <monishkumarr567@gmail.com>
2026-01-14 10:17:17 +0530

    fix: removed nested react app and react hooks folders

--- react hooks/react app/src/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-    return (
-        <footer className="footer">
-            <p>© 2026 My React App. All rights reserved.</p>
-        </footer>
-    )
-}
-
-export default Footer;
